158a5159a99dcb1a5958aa813366722825ad4b16
Amir Bagheri <32092233+KhodeAmir@users.noreply.github.com>
2019-10-19 02:59:11 +0330

    Update utils.lua

--- U-T/utils.lua
+++ U-T/utils.lua
@@ -2,7 +2,6 @@
 utf8 = require "U-T.utf8" 
   client = require("redis")
   socket = require('socket')
-
     redis = client.connect('127.0.0.1', 6379)
         serpent = require "serpent"
      encode_json = require("dkjson")
@@ -24,8 +23,8 @@ end
 configEnv = {}
 configEnv.data = {
   plist = {
-    "P-l",
-    "test"
+    "test",
+ 
   },
 }   
 getMainMessage=function (...)
@@ -78,6 +77,7 @@ end
 
 end
 
+SelfVersion = '\n*Version* _:_`|BETA => 2.1|`'
 dofile_ = function(filename)
 if io.open("./U-T/"..filename..'.lua' or '','r') ==nil then
  text =  'No file exists on this path !\n==> CRCO PROJET <=='
@@ -117,8 +117,38 @@ return val
 end
 
 threads = {}
-
-
+  getSixe = function(b)
+    local l = "B"
+    if b > 1024 then
+    b = b / 1024
+    l = "KB"
+    if b > 1024 then
+    b = b / 1024
+    l = "MB"
+    if b > 1024 then
+    b = b / 1024
+    l = "GB"
+    end
+    end
+        end
+        return string.format("%7.2f%2s",b,l)
+    end
+  MainGetMessage =  function (arg,co)
+      if co.messages then
+      for k ,v in pairs(co.messages) do
+        if v.sender_user_id then
+       tdbot.deleteChatMessagesFromUser(v.chat_id,v.sender_user_id,nil,nil)
+      end
+    
+    end
+      if co.messages and co.messages[1] and co.messages[1].chat_id and co.messages[1].id then
+       tdbot.getChatHistory(co.messages[1].chat_id, co.messages[1].id,0 , 100, MainGetMessage, nil)
+       end
+    
+       
+       end
+      
+      end
 function getTableSize(t)
   local count = 0
   for _, __ in pairs(t) do
@@ -133,7 +163,7 @@ local username_ = nil
   if g.username then
 username_ = '@'..g.username
   else
-  username_ = ''
+  username_ = g.first_name
   end
   end
 tdbot.getUser(user_id,getid,nil)
@@ -211,11 +241,43 @@ else
 end
   CreateFile(config , "./U-T/config.lua")
 end
-setex = function(val,name) 
-  time = os.time()
-
-  ---coming soon
+getUserStatus = function(status)
+  if status then
+  if status._ == 'userStatusOnline' then
+  PreStatus = '|Online|'
+  elseif status._ == 'userStatusLastMonth' then
+  PreStatus = '|Last Month|'
+  elseif status._ == 'userStatusLastWeek' then
+  PreStatus = '|Last Week|'
+  elseif status._ == 'userStatusRecently' then
+  PreStatus = '|Recently|'
+  elseif status._ == 'userStatusOffline' then
+  PreStatus= '|Offline|'
+  elseif status._ == 'userStatusEmpty' then
+  PreStatus = '|The user status was never changed|'
+  end
+else
+    PreStatus = '|Error 404|'
 end
+  return PreStatus
+  end
+  getUserType = function(type)
+    if type then
+    if type._ == 'userTypeRegular' then
+    PreType = '|userType|'
+    elseif type._ == 'userTypeDeleted' then
+    PreType = '|userDeleted|'
+    elseif type._ == 'userTypeBot' then
+    PreType = '|userBot|'
+    elseif type._ == 'userTypeUnknown' then
+    PreType = '|Unknown|'
+    end
+  else
+      PreType = '|Error 404|'
+  end
+    return PreType
+  end
+  
 returndata = function(value)
 if  value == nil  then
   return '|D|'
@@ -233,23 +295,56 @@ function save_array_test(tab)
     table[#table+1]=k
   end
   end
-sadd = function(val,name)
-  if not config.data[val] then
-    config.data[val] = {}
-  end
-  table.insert(config.data[val] ,name)
-  CreateFile(config , "./U-T/config.lua")
-end
+
+
 CHECK = function(VAL,NAME)
+  if config.data[VAL] then
   for k,v in pairs(config.data[VAL]) do
+ 
       if NAME == v then
           return k
       end
   end
+end
   return false
 end
+is_Saved = function(pth,name)
+
+    list = config.data[pth] or {}
+ 
+  
+         for v,value in pairs(list) do
+      
+
+          if name == value then
+      
+          var = false
+          else 
+            var = true
+         end
+        end
+
+        if #list == 0 then
+       
+          var = true
+        end    
+  
+      return var
+    
+end
+sadd = function(val,name)
+  if not config.data[val] then
+    config.data[val] = {}
+  end
+  if is_Saved(val,name)  then
+  table.insert(config.data[val] ,name)
+  CreateFile(config , "./U-T/config.lua")
+  end
+end
 sremove = function(val,name)
+if CHECK(val,name) then
   table.remove(config.data[val] ,CHECK(val,name))
+end
 end
 searchvl=function(y,x) 
      for _R,v in pairs(config[data][y]) do 
@@ -316,6 +411,43 @@ is_private = function(msg)
   chat_id = tostring(msg.chat_id)
   if chat_id:match('^-') then return false else return true end
   end
+  function Download(url, file_name)
+    print("url to download: "..url)
+    local respbody = {}
+    local options = {
+    url = url,
+    sink = ltn12.sink.table(respbody),
+    redirect = true
+    }
+    local response = nil
+    if url:match('^https') then
+    options.redirect = false
+    response = {https.request(options)}
+    else
+    response = {http.request(options)}
+    end
+    local code = response[2]
+    local headers = response[3]
+    local status = response[4]
+    if code ~= 200 then return nil end
+    file_name = file_name or get_http_file_name(url, headers)
+    local file_path = "./CRCO"..file_name
+    print("Saved to: "..file_path)
+    file = io.open(file_path, "w+")
+    file:write(table.concat(respbody))
+    file:close()
+    return file_path
+    end
+    is_supergroup = function(msg)
+chat_id = tostring(msg.chat_id)
+if chat_id:match('^-100') then 
+if not msg.is_channel_post then
+return true
+end
+else
+return false
+end
+end
   exists = function(nameFile)
   if type(nameFile)~="string" then return false end
   return os.rename(nameFile,nameFile) and true or false
